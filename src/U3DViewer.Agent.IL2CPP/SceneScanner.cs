b6f3f8e3964c62e538ba697d9e0c5fef760185f0
niu-row <niurow@outlook.com>
2026-09-03 22:37:46 +0800

    Fix IL2CPP incremental scanner accessibility

--- src/U3DViewer.Agent.IL2CPP/SceneScanner.cs
+++ src/U3DViewer.Agent.IL2CPP/SceneScanner.cs
@@ -174,7 +174,7 @@ internal static class SceneScanner
         }
     }
 
-    private readonly struct SceneScanWorkItem
+    internal readonly struct SceneScanWorkItem
     {
         public SceneScanWorkItem(GameObject gameObject, GameObjectInfo[] target, int index)
         {
