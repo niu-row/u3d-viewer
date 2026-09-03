ee15f92064f112f6fb2eb11c6d95028b095c9a53
niu-row <niurow@outlook.com>
2026-09-04 03:46:35 +0800

    Define NativeBridge ABI version

---
+++ src/U3DViewer.Protocol/NativeBridgeProtocol.cs
@@ -0,0 +1,7 @@
+namespace U3DViewer.Protocol;
+
+public static class NativeBridgeProtocol
+{
+    // Increment whenever the exported NativeBridge ABI changes incompatibly.
+    public const int AbiVersion = 1;
+}
