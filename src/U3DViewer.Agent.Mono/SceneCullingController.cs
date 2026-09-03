a5a8fffda3e3f02a81686bd03161a16fc5956311
niu-row <niurow@outlook.com>
2026-09-04 01:08:54 +0800

    Add Mono Scene culling mask controller

---
+++ src/U3DViewer.Agent.Mono/SceneCullingController.cs
@@ -0,0 +1,103 @@
+using U3DViewer.Protocol;
+using UnityEngine;
+
+namespace U3DViewer.Agent.Mono;
+
+internal sealed class SceneCullingController
+{
+    private const string ViewerCameraObjectName = "__U3DViewerCamera";
+
+    private readonly string[] _layerNames = CaptureLayerNames();
+    private SceneCullingMode _mode = SceneCullingMode.MainCamera;
+    private int _manualMask = -1;
+    private Camera? _viewerCamera;
+
+    public void Apply(ViewerCommand command)
+    {
+        if (command.Kind != ViewerCommandKind.CameraCullingMask)
+        {
+            return;
+        }
+
+        _mode = command.CullingMode;
+        if (_mode == SceneCullingMode.Manual)
+        {
+            _manualMask = command.CullingMask;
+        }
+
+        ApplyCurrentMode();
+    }
+
+    public void Reapply() => ApplyCurrentMode();
+
+    public void Populate(RenderTargetInfo? target)
+    {
+        if (target is null)
+        {
+            return;
+        }
+
+        if (_mode == SceneCullingMode.MainCamera)
+        {
+            ApplyCurrentMode();
+        }
+
+        var camera = FindViewerCamera();
+        target.CullingMode = _mode;
+        target.CullingMask = camera is null ? ResolveMask() : camera.cullingMask;
+        target.LayerNames = _layerNames;
+    }
+
+    private void ApplyCurrentMode()
+    {
+        var camera = FindViewerCamera();
+        if (camera is not null)
+        {
+            camera.cullingMask = ResolveMask();
+        }
+    }
+
+    private int ResolveMask()
+    {
+        return _mode switch
+        {
+            SceneCullingMode.All => -1,
+            SceneCullingMode.Manual => _manualMask,
+            _ => ResolveMainCameraMask()
+        };
+    }
+
+    private int ResolveMainCameraMask()
+    {
+        var main = Camera.main;
+        var viewer = FindViewerCamera();
+        return main is null || main == viewer ? -1 : main.cullingMask;
+    }
+
+    private Camera? FindViewerCamera()
+    {
+        if (_viewerCamera is not null)
+        {
+            return _viewerCamera;
+        }
+
+        var cameraObject = GameObject.Find(ViewerCameraObjectName);
+        if (cameraObject is null)
+        {
+            return null;
+        }
+
+        _viewerCamera = cameraObject.GetComponent<Camera>();
+        return _viewerCamera;
+    }
+
+    private static string[] CaptureLayerNames()
+    {
+        var names = new string[32];
+        for (var layer = 0; layer < names.Length; layer++)
+        {
+            names[layer] = LayerMask.LayerToName(layer) ?? string.Empty;
+        }
+        return names;
+    }
+}
