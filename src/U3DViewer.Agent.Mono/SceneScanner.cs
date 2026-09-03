dd3e4393c4fc0538881e2f6a1c724e12b2d596d0
niu-row <niurow@outlook.com>
2026-09-03 17:32:03 +0800

    feat(agent): capture live Unity scene hierarchy

---
+++ src/U3DViewer.Agent.Mono/SceneScanner.cs
@@ -0,0 +1,99 @@
+using U3DViewer.Protocol;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace U3DViewer.Agent.Mono;
+
+internal static class SceneScanner
+{
+    public static SceneSnapshot Capture(long sequence)
+    {
+        var scenes = new SceneInfo[SceneManager.sceneCount];
+
+        for (var i = 0; i < SceneManager.sceneCount; i++)
+        {
+            var scene = SceneManager.GetSceneAt(i);
+            var roots = scene.GetRootGameObjects();
+            var rootInfos = new GameObjectInfo[roots.Length];
+
+            for (var rootIndex = 0; rootIndex < roots.Length; rootIndex++)
+            {
+                rootInfos[rootIndex] = CaptureGameObject(roots[rootIndex]);
+            }
+
+            scenes[i] = new SceneInfo
+            {
+                BuildIndex = scene.buildIndex,
+                Name = scene.name ?? string.Empty,
+                IsLoaded = scene.isLoaded,
+                Roots = rootInfos
+            };
+        }
+
+        return new SceneSnapshot
+        {
+            Sequence = sequence,
+            UnixTimeMs = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
+            Scenes = scenes
+        };
+    }
+
+    private static GameObjectInfo CaptureGameObject(GameObject gameObject)
+    {
+        var transform = gameObject.transform;
+        var children = new GameObjectInfo[transform.childCount];
+
+        for (var i = 0; i < transform.childCount; i++)
+        {
+            children[i] = CaptureGameObject(transform.GetChild(i).gameObject);
+        }
+
+        var components = gameObject.GetComponents<Component>();
+        var componentNames = new List<string>(components.Length);
+        foreach (var component in components)
+        {
+            if (component != null)
+            {
+                componentNames.Add(component.GetType().FullName ?? component.GetType().Name);
+            }
+        }
+
+        return new GameObjectInfo
+        {
+            InstanceId = gameObject.GetInstanceID(),
+            Name = gameObject.name ?? string.Empty,
+            ActiveSelf = gameObject.activeSelf,
+            ActiveInHierarchy = gameObject.activeInHierarchy,
+            Layer = gameObject.layer,
+            Tag = ReadTag(gameObject),
+            Transform = new TransformInfo
+            {
+                Position = ToInfo(transform.position),
+                LocalPosition = ToInfo(transform.localPosition),
+                EulerAngles = ToInfo(transform.eulerAngles),
+                LocalScale = ToInfo(transform.localScale)
+            },
+            Components = componentNames.ToArray(),
+            Children = children
+        };
+    }
+
+    private static string ReadTag(GameObject gameObject)
+    {
+        try
+        {
+            return gameObject.tag ?? string.Empty;
+        }
+        catch (UnityException)
+        {
+            return string.Empty;
+        }
+    }
+
+    private static Vector3Info ToInfo(Vector3 value) => new()
+    {
+        X = value.x,
+        Y = value.y,
+        Z = value.z
+    };
+}
